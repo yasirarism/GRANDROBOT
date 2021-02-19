14dcaf0784d43431bd017d8a02cf9b3167d0cd7a
DIPANSHU <77918121+infotechbro@users.noreply.github.com>
2021-02-19 14:49:24 +0530

    Update admin.py

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -332,8 +332,8 @@ def adminlist(bot: Bot, update: Update):
         if user.username:
             name = name = escape_markdown("@" + user.username)
         if status == "creator":
-            text += "\n  ✪ 𝚆𝙾𝙷 𝚃𝙷𝙸𝚂 𝙾𝙿 𝙶𝚁𝙾𝚄𝙿 **𝙲𝚁𝙴𝙰𝚃𝙾𝚁** 𝙸𝚂 ✪:"
-            text += "\n` • `{} \n\n • *Administrators*:".format(name)
+            text += "\n✪ 𝚆𝙾𝙷 𝚃𝙷𝙸𝚂 𝙾𝙿 𝙶𝚁𝙾𝚄𝙿 **𝙲𝚁𝙴𝙰𝚃𝙾𝚁** 𝙸𝚂 ✪:"
+            text += "\n` • `{} \n\n• *Administrators*:".format(name)
     for admin in administrators:
         user = admin.user
         status = admin.status
